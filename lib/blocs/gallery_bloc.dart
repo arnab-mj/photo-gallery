87613cf948708d34ef3cc1e3a0e4d6bcba106dc4
Arnab <arnab.majumder96@outlook.com>
2022-11-21 02:40:18 +0600

    added pagination in photo gallery

--- lib/blocs/gallery_bloc.dart
+++ lib/blocs/gallery_bloc.dart
@@ -1,32 +1,74 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:photo_gallery/blocs/base_bloc.dart';
 import 'package:photo_gallery/models/photos_list_response.dart';
+import 'package:photo_gallery/network/api_base_helper.dart';
 import 'package:photo_gallery/network/api_response.dart';
 import 'package:photo_gallery/repositories/photos_repository.dart';
 
 class GalleryBloc extends BaseBloc {
   final PhotosRepository _repo = PhotosRepository();
   final StreamController<ApiResponse<List<Photo>>> _scPhotosList = StreamController<ApiResponse<List<Photo>>>();
+  final StreamController<ApiResponse<List<Photo>>> _scRequestNextPage = StreamController<ApiResponse<List<Photo>>>();
 
   StreamSink<ApiResponse<List<Photo>>> get photosListSink => _scPhotosList.sink;
   Stream<ApiResponse<List<Photo>>> get photosListStream => _scPhotosList.stream;
 
-  int pageNumber = 1;
+  StreamSink<ApiResponse<List<Photo>>> get requestNextPageSink => _scRequestNextPage.sink;
+  Stream<ApiResponse<List<Photo>>> get requestNextPageStream => _scRequestNextPage.stream;
 
-  void getPhotosList() async {
-    photosListSink.add(ApiResponse.loading());
+  final ScrollController _scrollController = ScrollController();
+  ScrollController get scrollController => _scrollController;
+
+  final List<Photo> _photoList = [];
+
+  List<Photo> get photoList => _photoList;
+
+  int pageNumber = 1;
+  bool hasNextPage = false;
+
+  GalleryBloc() {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent && !_scrollController.position.outOfRange && hasNextPage) {
+        requestNextPage();
+      }
+    });
+  }
+
+  void getPhotoList(StreamSink<ApiResponse<List<Photo>>> sink) async {
+    sink.add(ApiResponse.loading());
 
     try {
-      final List<Photo> response = (await _repo.getPhotosList(pageNumber)).body;
-      photosListSink.add(ApiResponse.completed(response));
+      final Response<List<Photo>> response = await _repo.getPhotosList(pageNumber);
+
+      _photoList.addAll(response.body);
+
+      sink.add(ApiResponse.completed(response.body));
+
+      if (response.headers['link'].toString().contains('rel="next"')) {
+        hasNextPage = true;
+        pageNumber++;
+      } else {
+        hasNextPage = false;
+      }
     } catch (e) {
-      photosListSink.add(ApiResponse.error(e.toString()));
+      sink.add(ApiResponse.error(e.toString()));
     }
+  }
+
+  void getInitialPhotosList() async {
+    getPhotoList(photosListSink);
+  }
+
+  void requestNextPage() async {
+    getPhotoList(requestNextPageSink);
   }
 
   @override
   void dispose() {
     _scPhotosList.close();
+    _scRequestNextPage.close();
+    _scrollController.dispose();
   }
 }
--- lib/views/gallery_screen/gallery_screen.dart
+++ lib/views/gallery_screen/gallery_screen.dart
@@ -1,18 +1,13 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:photo_gallery/blocs/gallery_bloc.dart';
 import 'package:photo_gallery/models/photos_list_response.dart';
 import 'package:photo_gallery/network/api_response.dart';
 import 'package:photo_gallery/utils/app_constant.dart';
 import 'package:photo_gallery/utils/utility.dart';
 import 'package:photo_gallery/views/gallery_screen/widgets/gallery_image_box.dart';
-import 'package:photo_gallery/views/photo_preview_screen/photo_preview_screen.dart';
-import 'package:photo_gallery/widgets/cached_image.dart';
 import 'package:photo_gallery/widgets/error.dart';
+
+import '../photo_preview_screen/photo_preview_screen.dart';
 
 class GalleryScreen extends StatefulWidget {
   static const String routeName = 'gallery-screen';
@@ -24,14 +19,22 @@ class GalleryScreen extends StatefulWidget {
 }
 
 class _GalleryScreenState extends State<GalleryScreen> {
-  final ScrollController _scrollController = ScrollController();
   final GalleryBloc _bloc = GalleryBloc();
 
   @override
   void initState() {
     super.initState();
 
-    _bloc.getPhotosList();
+    _bloc.requestNextPageStream.listen((event) {
+      if (event.status == Status.loading) {
+      } else if (event.status == Status.completed) {
+        setState(() {});
+      } else if (event.status == Status.error) {
+        showSnackBar(context, event.message, true);
+      }
+    });
+
+    _bloc.getInitialPhotosList();
   }
 
   @override
@@ -39,29 +42,6 @@ class _GalleryScreenState extends State<GalleryScreen> {
     super.dispose();
 
     _bloc.dispose();
-    _scrollController.dispose();
-  }
-
-  Widget photosGridView(List<Photo>? photosList) {
-    return GridView.builder(
-      controller: _scrollController,
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.pushNamed(context, PhotoPreviewScreen.routeName, arguments: PhotoPreviewScreenArgs(photoList: _bloc.photoList, index: index));
-          },
-          child: GalleryImageBox(photo: _bloc.photoList[index]),
-        );
-      },
-      itemCount: photosList?.length ?? 0,
-      primary: false,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: (MediaQuery.of(context).size.width / AppConstant.galleryThumbnailSize).round(),
-        crossAxisSpacing: AppConstant.galleryCrossAxisSpacing,
-        mainAxisSpacing: AppConstant.galleryMainAxisSpacing,
-      ),
-      scrollDirection: Axis.vertical,
-    );
   }
 
   Widget rootWidget() {
@@ -73,13 +53,41 @@ class _GalleryScreenState extends State<GalleryScreen> {
             child: showLoader(context),
           );
         } else if (snapshot.data?.status == Status.completed) {
-          return photosGridView(snapshot.data?.data);
+          return photosGridView();
         } else if (snapshot.data?.status == Status.error) {
-          return Error(errorMessage: snapshot.data?.message, onRetryPressed: () => _bloc.getPhotosList());
+          return Error(errorMessage: snapshot.data?.message, onRetryPressed: () => _bloc.getInitialPhotosList());
         }
 
         return const SizedBox.shrink();
       },
+    );
+  }
+
+  Widget photosGridView() {
+    return GridView.builder(
+      controller: _bloc.scrollController,
+      itemBuilder: (context, index) {
+        return InkWell(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                PhotoPreviewScreen.routeName,
+                arguments: PhotoPreviewScreenArgs(
+                  photoList: _bloc.photoList,
+                  index: index,
+                ),
+              );
+            },
+            child: GalleryImageBox(photo: _bloc.photoList[index]),
+        );
+      },
+      itemCount: _bloc.photoList.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: (MediaQuery.of(context).size.width / AppConstant.galleryThumbnailSize).round(),
+        crossAxisSpacing: AppConstant.galleryCrossAxisSpacing,
+        mainAxisSpacing: AppConstant.galleryMainAxisSpacing,
+      ),
+      scrollDirection: Axis.vertical,
     );
   }
 
